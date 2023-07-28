d35da4709875708df4aed6ca3550c9955355d2f6
unknown <pnjukia@gmail.com>
2023-07-28 16:40:17 +0300

    main.java file updates

--- src/main/java/services/HeroService.java
+++ src/main/java/services/HeroService.java
@@ -3,7 +3,7 @@ package services;
 import java.util.ArrayList;
 import java.util.List;
 import models.Hero;
-//a
+//
 public class HeroService {
     private List <Hero> heroList = new ArrayList<>();
 
